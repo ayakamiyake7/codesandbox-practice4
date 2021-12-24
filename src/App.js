c588086b4574fb6621f2e28c4ddeae055c83b7be
ayakamiyake7 <88525743+ayakamiyake7@users.noreply.github.com>
2021-12-24 14:14:13 +0000

    Add templates.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import { BrowserRouter } from "react-router-dom";
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
 import { SearchInput } from "./components/molecules/SearchInput";
 import { UserCard } from "./components/organisms/user/UserCard";
 import "./styles.css";
+import { DefaultLayout } from "./templates/DefaultLayout";
+import { HeaderOnly } from "./templates/HeaderOnly";
 
 const ayaka = {
   name: "Ayaka",
@@ -17,13 +20,17 @@ const ayaka = {
 
 export default function App() {
   return (
-    <div className="App">
-      <PrimaryButton>1</PrimaryButton>
-      <PrimaryButton>Test</PrimaryButton>
-      <SecondaryButton>Button</SecondaryButton>
-      <br />
-      <SearchInput />
-      <UserCard user={ayaka} />
-    </div>
+    <BrowserRouter>
+      {/* <HeaderOnly> */}
+      <DefaultLayout>
+        <PrimaryButton>1</PrimaryButton>
+        <PrimaryButton>Test</PrimaryButton>
+        <SecondaryButton>Button</SecondaryButton>
+        <br />
+        <SearchInput />
+        <UserCard user={ayaka} />
+        {/* </HeaderOnly> */}
+      </DefaultLayout>
+    </BrowserRouter>
   );
 }
